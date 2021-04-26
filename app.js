cb45e7daf68139c52a8352f6e3af74f58e9ed1c0
francisdailig <francisdailig@francisdailigui-MacBookPro.local>
2021-04-27 06:39:47 +0900

    New Templates for HTML Front End

--- app.js
+++ app.js
@@ -5,53 +5,58 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 31337;                 // Set a port number at the top so it's easy to change in the future
+//PORT        = 31337;                 // Set a port number at the top so it's easy to change in the future
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', handlebars);
+app.set('port', 31337);
+
 var db = require('./database/db_connector');
 
 /*
     ROUTES
 */
-
 /*
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
     {
         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
     });                                         // requesting the web site.
 */
+app.get('/', function(req, res){
+	res.render('index.handlebars');
+});
 
-app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+app.get('/index.html', function(req, res){
+	res.render('index.handlebars');
+});
+app.get('/workstations', function(req, res){
+	var testData = [
+		{'name': 'dailigMac', 'os': 'MacOS', 'ip': '192.168.1.2'},
+		{'name': 'walshMac', 'os': 'MacOS', 'ip': '192.168.1.3'}
+	];
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+	var context = {};
+	context.dataList = testData
+	res.render('workstations.handlebars', context);
+});
+app.get('/view2', function(req, res){
+	res.render('view2.handlebars');
+});
+app.get('/view3', function(req, res){
+	res.render('view3.handlebars');
+});
+app.get('/view4', function(req, res){
+	res.render('view4.handlebars');
+});
+app.get('/view5', function(req, res){
+	res.render('view5.handlebars');
+});
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+app.listen(app.get('port'), function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.')
 });
 
--- database/db_connector.js
+++ database/db_connector.js
@@ -6,9 +6,9 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_dailigf',
-    password        : '3117',
+    host            : 'localhost',
+    user            : 'francisdailig',
+    password        : 'tlIm$I$nw23',
     database        : 'cs340_dailigf'
 })
 
