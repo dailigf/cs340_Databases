5db188e2c33351f65a86e315b35f939bf82bd7bb
D-Walsh33 <62631882+D-Walsh33@users.noreply.github.com>
2021-05-25 15:01:50 +0900

    some small changes

--- addresses.js
+++ addresses.js
@@ -1,5 +1,3 @@
-const { query } = require('express');
-
 module.exports = function () {
     var express = require('express');
     var router = express.Router();
--- applications.js
+++ applications.js
@@ -3,24 +3,26 @@ module.exports = function(){
     var router = express.Router();
 
     function getApplications(res, mysql, context, complete){
-        var query = "SELECT * FROM Applications";
+        var query = "SELECT appID, appName, appType FROM Applications";
         mysql.pool.query(query, function(error, results, fields){
 			if(error){
 				res.write(JSON.stringify(error));
 				res.end();
 			}
 			context.applications = results;
+            console.log(context.applications);
 			complete();
 		})
     };
 
     router.get('/', function(req, res){
+        console.log("inside router.get for applications");
         var callbackcount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
         getApplications(res, mysql, context, complete);
         function complete(){
-            callbackcount++
+            callbackcount++;
             if(callbackcount >= 1){
                 res.render('applications.handlebars', context);
             }
