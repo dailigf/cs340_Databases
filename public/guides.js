cd6c7522f417713b001beb6c50306c7729745903
francisdailig <francisdailig@francisdailigui-MacBookPro.local>
2021-05-30 15:55:16 +0900

    Updated guides.js to ensure that delete is propagated to the control instances table.  Added comments to all the .js files

--- public/guides.js
+++ public/guides.js
@@ -1,4 +1,6 @@
 function deleteGuide(id){
+	/*Function that is called when the delete button clicked.  Invoked the '/guides/:id' route.
+	 * Appends id parameter*/
 	$.ajax({
         	url: '/guides/' + id,
         	type: 'DELETE',
@@ -9,6 +11,8 @@ function deleteGuide(id){
 }
 
 function updateGuide(id){
+	/*Function that is called when the update button is clicked.  Serialized the user input and POSTs
+	 * to the '/guides/:id' route*/
 	$.ajax({
 		url: '/guides/' + id,
 		type: 'POST',
